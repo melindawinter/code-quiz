3b57503dae1f3bad3a2c3529b2d39a4309c6762a
Melinda Winter <melindawinter42@gmail.com>
2020-06-01 16:43:17 -0700

    added header and links

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,27 @@
-//Use an array of objects for questions (said in class)
+var timeEl = document.querySelector(".time");
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 75;
+
+function setTime() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left until time is up!";
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+  }, 1000);
+}
+
+function sendMessage() {
+  timeEl.textContent = " ";
+
+  var imgEl = document.createElement("img");
+
+  imgEl.setAttribute("src", "images/image_1.jpg");
+  mainEl.appendChild(imgEl);
+}
+
+setTime();
