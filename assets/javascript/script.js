8e677dfe5f258383d011d1e42dcad2f4bca84a4c
Melinda Winter <melindawinter42@gmail.com>
2020-06-02 15:11:57 -0700

    working on js file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,66 @@
 //Declare variables
-const startButton = document.getElementById("start-btn");
-const questionContainerEl = document.getElementById("question-container");
-const quizTimerEl = document.getElementById("timer");
+var startButton = document.getElementById("start-btn");
+var questionContainerEl = document.getElementById("question-container");
+var quizTimerEl = document.getElementById("timer");
+var questions = [
+  {
+    question: "Which of these is an accurate description of JavaScript?",
+    answerChoices: [
+      { text: "A delicious coffee drink", correct: false },
+      {
+        text:
+          "A computer programming language used to create interactive effects in web browsers",
+        correct: true,
+      },
+      {
+        text:
+          "A language used to define color, fonts, text alignment, borders, and other stylistic elements",
+        correct: false,
+      },
+      {
+        text: "A computer language indistinguishable from Java",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Which HTML tag is used to embed JavaScript?",
+    answerChoices: [
+      { text: "<script>", correct: true },
+      { text: "<java>", correct: false },
+      { text: "<barista>", correct: false },
+      { text: "<js>", correct: false },
+    ],
+  },
+  {
+    question: "How would you write the message 'I love cats' in an alert box?",
+    answerChoices: [
+      { text: "prompt('I love cats');", correct: false },
+      { text: "fancybox('I love cats');", correct: false },
+      { text: "alert('I love cats');", correct: true },
+      { text: "espresso('I love cats');", correct: false },
+    ],
+  },
+  {
+    question: "Which of these is a proper way to create a function?",
+    answerChoices: [
+      { text: "console.log('thisFunction')", correct: false },
+      { text: "alert(function)", correct: false },
+      { text: "function:thisFunction", correct: false },
+      { text: "function thisFunction()", correct: true },
+    ],
+  },
+  {
+    question: "How do you write a single line comment in JavaScript?",
+    answerChoices: [
+      { text: "Yell at your computer", correct: false },
+      { text: "//Write a comment", correct: true },
+      { text: "<!--Write a comment-->", correct: false },
+      { text: "Append a Notes page", correct: false },
+    ],
+  },
+];
+
 //Event listener for Start button
 startButton.addEventListener("click", startQuiz);
 
