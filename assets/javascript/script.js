45b4bb0d1ba1b7c9c3de9e772284a319f2eeb2e7
Melinda Winter <melindawinter42@gmail.com>
2020-06-03 13:46:05 -0700

    working on js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,58 +1,70 @@
 //Declare variables
 var startButton = document.getElementById("start-btn");
+var nextButton = document.getElementById("next-btn");
 var questionContainerEl = document.getElementById("question-container");
 var quizTimerEl = document.getElementById("timer");
-var questionEl = document.getElementById("question-text");
-var answersEl = document.getElementById("answer-choices");
+var questionEl = document.getElementById("question");
+var answerButtonsEl = document.getElementById("answer-buttons");
 var questions = [
   {
     question: "Which of these is an accurate description of JavaScript?",
-    choices: [
-      "A delicious coffee drink",
-      "A computer programming language used to create interactive effects in web browsers",
-      "A language used to define color, fonts, text alignment, borders, and other stylistic elements",
-      "A computer language indistinguishable from Java",
+    answers: [
+      { text: "A delicious coffee drink", correct: false },
+      {
+        text:
+          "A computer programming language used to create interactive effects in web browsers",
+        correct: true,
+      },
+      {
+        text:
+          "A language used to define color, fonts, text alignment, borders, and other stylistic elements",
+        correct: false,
+      },
+      {
+        text: "A computer language indistinguishable from Java",
+        correct: false,
+      },
     ],
-    answer:
-      "A computer programming language used to create interactive effects in web browsers",
   },
 
   {
     question: "Which HTML tag is used to embed JavaScript?",
-    choices: ["<script>", "<java>", "<barista>", "<js>"],
-    answer: "<script>",
+    answers: [
+      { text: "<script>", correct: true },
+      { text: "<java>", correct: false },
+      { text: "<barista>", correct: false },
+      { text: "<js>", correct: false },
+    ],
   },
   {
     question: "How would you write the message 'I love cats' in an alert box?",
-    choices: [
-      "prompt('I love cats');",
-      "fancybox('I love cats');",
-      "alert('I love cats');",
-      "espresso('I love cats');",
+    answers: [
+      { text: "prompt('I love cats');", correct: false },
+      { text: "fancybox('I love cats');", correct: false },
+      { text: "alert('I love cats');", correct: true },
+      { text: "espresso('I love cats');", correct: false },
     ],
-    answer: "alert('I love cats');",
   },
   {
     question: "Which of these is a proper way to create a function?",
-    choices: [
-      "console.log('thisFunction')",
-      "alert(function)",
-      "function:thisFunction",
-      "function thisFunction()",
+    answers: [
+      { text: "console.log('thisFunction')", correct: false },
+      { text: "alert(function)", correct: false },
+      { text: "function:thisFunction", correct: false },
+      { text: "function thisFunction()", correct: true },
     ],
-    answer: "function thisFunction()",
   },
   {
     question: "How do you write a single line comment in JavaScript?",
-    choices: [
-      "Yell at your computer",
-      "//Write a comment",
-      "<!--Write a comment-->",
-      "Append a Notes page",
+    answers: [
+      { text: "Yell at your computer", correct: false },
+      { text: "//Write a comment", correct: true },
+      { text: "<!--Write a comment-->", correct: false },
+      { text: "Append a Notes page", correct: false },
     ],
-    answer: "//Write a comment",
   },
 ];
+var shuffledQuestions, currentQuestionIndex;
 
 //Event listener for Start button
 startButton.addEventListener("click", startQuiz);
@@ -62,21 +74,64 @@ function startQuiz() {
   console.log("started");
   startButton.classList.add("hide");
   questionContainerEl.classList.remove("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
   quizTimerEl.classList.remove("hide");
-  showQuestion();
+  setNextQuestion();
 }
 
-// Set next question
+// Randomly select next question
 
-function showQuestion() {
-  for (var i = 0; i > questions.length; i++) {
-    questionEl.textContent = questions.question;
+function setNextQuestion() {
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+//Display the question and answer choices
+function showQuestion(question) {
+  questionEl.innerText = question.question;
+  question.answers.forEach((answer) => {
+    var button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsEl.appendChild(button);
+  });
+}
+
+function resetState() {
+  nextButton.classList.add("hide");
+  while (answerButtonsEl.firstChild) {
+    answerButtonsEl.removeChild(answerButtonsEl.firstChild);
   }
 }
 
 // Select answer
+function selectAnswer(e) {
+  var selectedAnswer = e.target;
+  var correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsEl.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  nextButton.classList.remove("hide");
+}
 
-//function selectAnswer() {}
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
 
 //Timer
 var timeEl = document.querySelector("#time");
